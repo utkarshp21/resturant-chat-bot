cf9e557ccb6db42a4660f874811236b055b8c0b3
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-03-04 02:53:42 -0500

    added amplify for aws sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { SecureComponent } from './secure/secure.component';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    SecureComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AmplifyAngularModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AmplifyService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
