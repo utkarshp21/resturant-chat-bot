24206ac8a3bb8c618e4670c5fc48fff04a979e80
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-03-04 19:51:57 -0500

    fetching loged in user details

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ChatbotService } from './chatbot.service';
 import { Router } from '@angular/router';
-import { Message } from '../message';
+import { Message } from './types/message';
+import User from './types/user';
 import { AmplifyService } from 'aws-amplify-angular';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-chat-bot',
@@ -13,8 +15,20 @@ export class ChatBotComponent {
 
   title = 'Chat Bot';
   messages: Message[] = [];
+  user: User;
 
   constructor(private chatBotService: ChatbotService, private amplifyService: AmplifyService, public router: Router) {
+  }
+
+  ngOnInit() {
+    Auth.currentSession().then(session => {
+      this.user = {
+        username: session.getIdToken().payload["cognito:username"],
+        email: session.getIdToken().payload['email'],
+        phone_number: session.getIdToken().payload['phone_number'],
+        cognitoId: session.getIdToken().payload['sub'],
+      };
+    });
   }
 
   private getChatResponse(queryMessage: string): void {
---
+++ src/app/chat-bot/types/user.ts
@@ -0,0 +1,8 @@
+type User = {
+    cognitoId: string,
+    username: string,
+    email: string,
+    phone_number: string,
+};
+
+export default User;
