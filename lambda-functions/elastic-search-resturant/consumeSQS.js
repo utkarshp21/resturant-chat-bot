dc8bbb51acb247a0c056fbdb8c73180abbf80115
ashimagg <ashim.agg93@gmail.com>
2019-04-15 23:23:21 -0400

    refactor consumeSQS

--- lambda-functions/elastic-search-resturant/consumeSQS.js
+++ lambda-functions/elastic-search-resturant/consumeSQS.js
@@ -1,14 +1,9 @@
 
 const AWS = require('aws-sdk')
 const region = 'us-east-1'
-const sns_topic_arn = 'arn:aws:sns:us-east-1:245491298808:dynamodb'
 const sqs = new AWS.SQS({ region: region });
-AWS.config.update({region: region});
-
+const queueURL = "https://sqs.us-east-1.amazonaws.com/245491298808/resturantQueue"
 module.exports = function (event) {
-
-    let queueURL = "https://sqs.us-east-1.amazonaws.com/245491298808/resturantQueue"
-
     let params = {
         AttributeNames: [
             "SentTimestamp"
@@ -32,32 +27,24 @@ module.exports = function (event) {
         sqs.receiveMessage(params, function (err, data) {
 
             if (err) {
-
                 reject("SQS Recieve Error");
 
             } else if (data.Messages) {
-
-                let sqsMessage = JSON.parse(data.Messages[0].Body);
-                
+                let sqsMessage = JSON.parse(data.Messages[0].Body);                
                 let deleteParams = {
                     QueueUrl: queueURL,
                     ReceiptHandle: data.Messages[0].ReceiptHandle
                 };
-
                 sqs.deleteMessage(deleteParams, function (err, data) {
                     if (err) {
                         reject("delete error");
-                    } else {
-                        
+                    } else {                        
                         // Create promise and SNS service object
                         resolve(sqsMessage);
                     }
                 });
-
             } else {
-
                 reject("Queue Empty");
-
             }
         });
 
